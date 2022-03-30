8e8eb8721d12742878f1b38d73f8890e76668e6d
De1337NY <shepm09@gmail.com>
2022-03-30 12:25:05 -0400

    Add files via upload
    Powershell script to send an email out upon an event. Run the commented code part first to save the creds as an exported xml file. Then set up a Task Scheduler to go off and trigger this script via powershell.exe when an event occurs, i.e. a file has been blocked by FSRM or a user is added to Domain Admins group

---
+++ General IT Scripts/EmailScript.ps1
@@ -0,0 +1,11 @@
+﻿	#$credential = get-credential
+    #$credential | Export-Clixml -path C:\secrets\emailcreds.xml
+
+    $EmailTo = "ToNotify@email.com"
+	$EmailFrom = "From@email.com"
+    $credential = Import-Clixml -Path  C:\secrets\emailcreds.xml
+	$Subject = "Blocked File was attempted to be created in Shares Directory"
+	$Body = "A blocked file type was attempted to be created in the Shares. A detailed warning is available in the Event Log under WindowsLogs\Applications\Warning Source SRMSVC"
+	$SMTPServer = "smtp.server.com"
+
+Send-MailMessage -Credential $credential -to $EmailTo -From $EmailFrom -Subject $Subject -Body $Body -SmtpServer $SMTPServer -UseSsl -port 587
