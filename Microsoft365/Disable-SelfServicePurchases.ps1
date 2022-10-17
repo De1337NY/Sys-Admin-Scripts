c93097cdafdaa4889f249b48d95d836e316c88f9
jantari <jantari@github.com>
2022-10-17 23:08:02 +0200

    add script to disable self-service purchases

---
+++ Microsoft365/Disable-SelfServicePurchases.ps1
@@ -0,0 +1,17 @@
+﻿<#
+	.DESCRIPTION
+	This script disables all user self-service purchases in M365
+#>
+
+#Requires -Modules MSCommerce
+
+Import-Module -Name MSCommerce
+
+Connect-MSCommerce
+
+$products = Get-MSCommerceProductPolicies -PolicyId AllowSelfServicePurchase
+
+foreach ($product in $products) {
+    Update-MSCommerceProductPolicy -PolicyId AllowSelfServicePurchase -ProductId $product.ProductId -Enabled $false
+}
+
